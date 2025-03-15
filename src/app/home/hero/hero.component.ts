8e396fde9ac64ed4bb66506380f92df550aebf35
w.alexander <will_alexander@me.com>
2025-03-15 10:30:11 +0100

    Move hero card styles to own file

--- src/app/home/hero/hero.component.ts
+++ src/app/home/hero/hero.component.ts
@@ -6,6 +6,7 @@ import { CtaButtonComponent } from './cta-button/cta-button.component';
   imports: [
     CtaButtonComponent
   ],
+  styleUrls: ['./hero.component.scss'],
   template: `
 		<section id="home" class="hero">
 			<h1>Level Up Your<br>Web Development Skills</h1>
@@ -13,45 +14,6 @@ import { CtaButtonComponent } from './cta-button/cta-button.component';
       <app-cta-button>Start Learning Today</app-cta-button>
 		</section>
   `,
-  styles: `
-    .hero {
-      text-align: center;
-      padding: 10rem 1rem 6rem;
-      background: linear-gradient(135deg, #0f172a 0%, #1e293b 100%);
-      position: relative;
-      overflow: hidden;
-    }
-
-    .hero::before {
-      content: '';
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      background: url("data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11 18c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm48 25c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm-43-7c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm63 31c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM34 90c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm56-76c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM12 86c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm28-65c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm23-11c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-6 60c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm29 22c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zM32 63c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm57-13c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-9-21c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM60 91c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM35 41c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM12 60c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2z' fill='%23818cf8' fill-opacity='0.1' fill-rule='evenodd'/%3E%3C/svg%3E");
-      opacity: 0.5;
-    }
-
-    h1 {
-      font-size: 4rem;
-      font-weight: 800;
-      margin-bottom: 1.5rem;
-      color: white;
-      line-height: 1.2;
-      position: relative;
-    }
-
-    p {
-      font-size: 1.5rem;
-      margin-bottom: 3rem;
-      color: #e2e8f0;
-      max-width: 600px;
-      margin-left: auto;
-      margin-right: auto;
-      position: relative;
-    }
-  `
 })
 export class HeroComponent {
 
