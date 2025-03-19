b970170049147dd000e1f2ddb1778e874dedd25e
w.alexander <will_alexander@me.com>
2025-03-19 15:44:02 +0100

    Complete activity

--- src/app/profile/profile-form/profile-form.component.ts
+++ src/app/profile/profile-form/profile-form.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ProfileService } from '../profile.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-profile-form',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './profile-form.component.html',
   styleUrls: ['./profile-form.component.scss']
 })
 export class ProfileFormComponent {
+  private formBuilder = inject(FormBuilder);
+  private profileService = inject(ProfileService);
+  profileForm = this.formBuilder.group({
+    name: [''],
+    title: [''],
+    bio: [''],
+    website: [''],
+    github: ['']
+  });
 
+  constructor() {
+    this.profileForm.valueChanges.pipe(
+      takeUntilDestroyed(),
+      tap(profile => this.profileService.setProfile(profile))
+    ).subscribe();
+  }
 }
