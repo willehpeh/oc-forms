cda9a1ed125433e94043e300d565a48d7834e90c
w.alexander <will_alexander@me.com>
2025-03-19 09:30:53 +0100

    Finished chapter

--- src/app/home/newsletter/newsletter.component.ts
+++ src/app/home/newsletter/newsletter.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-newsletter',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   styleUrls: ['./newsletter.component.scss'],
   template: `
 		<section class="newsletter">
 			<div class="newsletter-container">
 				<h2>Stay Updated</h2>
 				<p>Get the latest web development tips and tutorials in your inbox</p>
-				<form class="newsletter-form">
-					<input type="email" placeholder="Enter your email" required>
+				<form #newsletterForm="ngForm"
+              (ngSubmit)="onSubmit(newsletterForm)"
+              class="newsletter-form">
+					<input type="email"
+                 name="userEmail"
+                 [(ngModel)]="userEmail" 
+                 placeholder="Enter your email" 
+                 required>
 					<button type="submit">Subscribe</button>
 				</form>
 			</div>
@@ -18,5 +27,9 @@ import { Component } from '@angular/core';
   `,
 })
 export class NewsletterComponent {
+  userEmail: string = 'me@my-house.com';
 
+  onSubmit(form: NgForm): void {
+    console.log(form.value);
+  }
 }
