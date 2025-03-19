0c9916e54042fea7ae8f73ea6ad9c29965e6aa83
w.alexander <will_alexander@me.com>
2025-03-19 10:33:11 +0100

    Activity complete

--- src/app/home/skill-assessment/skill-assessment.component.ts
+++ src/app/home/skill-assessment/skill-assessment.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-skill-assessment',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './skill-assessment.component.html',
   styleUrls: ['./skill-assessment.component.scss']
 })
 export class SkillAssessmentComponent {
+  skillLevel: string = '';
 
+  onSubmitForm(form: NgForm): void {
+    console.log(form.value);
+  }
 }
