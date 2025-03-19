e9fcc0254324de3047739accc321da729bf6de0b
w.alexander <will_alexander@me.com>
2025-03-19 10:23:19 +0100

    Show contact form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,18 +2,21 @@ import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { CoursesComponent } from './courses/courses.component';
 import { NewsletterComponent } from './newsletter/newsletter.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-home',
   imports: [
     HeroComponent,
     CoursesComponent,
-    NewsletterComponent
+    NewsletterComponent,
+    ContactComponent
   ],
   template: `
     <app-hero/>
     <app-newsletter/>
     <app-courses/>
+    <app-contact/>
   `,
   styles: ``
 })
